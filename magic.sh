2d25269bacfdc0f0dc44e9e3e6247144503b28f7
jpca999 <jpca999@gmail.com>
2016-12-23 16:23:53 -0500

    -

--- magic.sh
+++ magic.sh
@@ -3,7 +3,7 @@
 sudo rm -rf ../index.html ../styles/ ../scripts/ ../img/ ../fonts/
 
 
-sudo cp -R Blog  ../Blog
+sudo cp -R app/Blog/ ../Blog
 
 
 sudo cp app/index.html  ../index.html
@@ -14,3 +14,5 @@ sudo cp -R app/styles/ ../styles/
 sudo cp -R app/scripts/ ../scripts/
 
 sudo cp -R app/img/ ../img/
+
+sudo cp -R app/fonts/ ../fonts/
