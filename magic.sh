0ad214de29eebfefd3d754329f1455241ec69867
jpca999 <jpca999@gmail.com>
2016-12-22 22:06:18 -0500

    -

---
+++ magic.sh
@@ -0,0 +1,14 @@
+cp app/index.html ../index2.html
+
+
+sudo rm -rf ../index.html ../styles/ ../scripts/ ../img/ ../fonts/
+
+
+sudo cp app/index.html  ../index.html
+
+
+sudo cp -R app/styles/ ../styles/
+
+sudo cp -R app/scripts/ ../scripts/
+
+sudo cp -R app/img/ ../img/
